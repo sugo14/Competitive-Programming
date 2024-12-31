4162184c59838557b7743474dbbfa6ac943ad444
sugo14 <98863576+sugo14@users.noreply.github.com>
2024-12-30 17:43:14 -0700

    Cleaned LeetCode 983 solution

--- leetcode/completed/983.cpp
+++ leetcode/completed/983.cpp
@@ -8,9 +8,7 @@ class Solution {
 public:
     int mincostTickets(vector<int>& days, vector<int>& costs) {
         for (int i = 0; i < days.size(); i++) { travel[days[i]] = 1; }
-        for (int i = 1; i <= 400; i++) {
-            dp[i] = min((travel[i] == 0) ? dp[i - 1] : (dp[i - 1] + costs[0]), min((i - 7 >= 0 ? (dp[i - 7] + costs[1]) : INT_MAX), (i - 30 >= 0 ? (dp[i - 30] + costs[2]) : INT_MAX)));
-        }
+        for (int i = 1; i <= 400; i++) { dp[i] = min((travel[i] == 0) ? dp[i - 1] : (dp[i - 1] + costs[0]), min(dp[max(0, i - 7)] + costs[1], dp[max(0, i - 30)] + costs[2])); }
         return dp[400];
     }
 };
